65623e943015d41ebd8755eed9d138599e0be0dd
mahaker <hideaki.matsunami@esm.co.jp>
2016-09-28 10:43:23 +0900

    refactoring

--- HW01-mahaker/js/doCulcSalary.js
+++ HW01-mahaker/js/doCulcSalary.js
@@ -1,20 +1,35 @@
 var olympic = "2020-07"; //ハイフンを区切りとした年月
+var now = new Date();
 
 function doPrintParseDist() {
-  olympicSprit = olympic.split("-");
+  olympicSprit = getOlympic();
   var output_olympic_message = "次回オリンピック開催は "+olympicSprit[0]+"年"+olympicSprit[1]+"月 です";
 	document.getElementById("dist_date").innerText = output_olympic_message;
 }
 
 function doCulcSalaryOlympic() {
-  var now = new Date();
-  var nowISO = now.toISOString();
-	var nowISOSprit = nowISO.split("-");
-  var span_year = olympicSprit[0] - nowISOSprit[0];
-  var span_month = olympicSprit[1] - nowISOSprit[1];
+	nowISOSprit = getNowISO();
+  var span_year = getSpan_year();
+  var span_month = getSpan_month();
   var total_span_month = span_year * 12 + span_month;
   var result_culc_salary = total_span_month * document.forms.form1.salary.value;
   document.getElementById("culc_salary_olympic").innerText = result_culc_salary;
+}
+
+function getNowISO() {
+  return now.toISOString().split("-");
+}
+
+function getOlympic() {
+  return olympic.split("-");
+}
+
+function getSpan_year() {
+  return olympicSprit[0] - nowISOSprit[0];
+}
+
+function getSpan_month() {
+  return olympicSprit[1] - nowISOSprit[1];
 }
 
 function maskInputNum() {
